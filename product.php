0c2476fd59f4053c21ad60aebe53fabea2f2122f
wailekfong <149465823+wailekfong@users.noreply.github.com>
2024-05-20 14:48:41 +0800

    css

--- product.php
+++ product.php
@@ -24,8 +24,47 @@ session_start();
     <link rel="stylesheet" href="product.css">
     <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl嗨⢾姞筗㵁縓狙혲絷䶜黺ࢌ鯇똯==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .card-container {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+            justify-content: center;
+            padding: 20px;
+        }
+        .card {
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            width: 300px;
+            text-align: center;
+            background-color: #fff;
+        }
+        .card img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 4px;
+        }
+        .card .product-id,
+        .card .product-name,
+        .card .product-price,
+        .card .product-stock,
+        .card .product-details {
+            margin-bottom: 10px;
+        }
+        .card .buy-now {
+            display: inline-block;
+            padding: 10px 20px;
+            color: #fff;
+            background-color: #28a745;
+            border-radius: 4px;
+            text-decoration: none;
+        }
+    </style>
+</head>
 <body>
-<header class="header_area sticky-header">
+    <header class="header_area sticky-header">
         <div class="main_menu">
             <nav class="navbar navbar-expand-lg navbar-light main_box">
                 <div class="container">
@@ -51,8 +90,8 @@ session_start();
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                                     aria-expanded="false">About Us</a>
                                 <ul class="dropdown-menu">
-                                    <li class="nav-item"><a class="nav-link" href="contactus.php">Contact Us</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="aboutus.php">About Us</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item submenu dropdown">
@@ -90,40 +129,27 @@ session_start();
             </div>
         </div>
     </header>
-    
+
     <h1>List of Products</h1>
-
-    <table border="1" width="650px">
-        <tr>
-            <th>Product ID</th>
-            <th>Product Name</th>
-            <th>Product Price</th>
-            <th>Product Stock</th>
-            <th>Product Size</th>
-            <th>Product Image</th>
-            <th>Product Details</th>    
-            <th>Action</th>
-        </tr>
-
+    <div class="card-container">
         <?php
         // Fetch and display products from database
         $result = mysqli_query($connect, "SELECT * FROM product");  
         while($row = mysqli_fetch_assoc($result)) {
         ?>          
-            <tr>
-                <td><?php echo $row["product_id"]; ?></td>
-                <td><?php echo $row["product_name"]; ?></td>
-                <td><?php echo $row["product_price"]; ?></td>
-                <td><?php echo ($row["product_stock"] > 0) ? "In Stock" : "Out of Stock"; ?></td>
-                <td><?php echo $row["product_size"]; ?></td>
-                <td><img src="<?php echo $row["product_image"]; ?>" alt="Product Image" style="width: 100px;"></td>
-                <td><?php echo $row["product_details"]; ?></td>
-                <td><a href="purchase.php?buy&productid=<?php echo $row['product_id']; ?>">Buy Now</a></td>
-            </tr>
+            <div class="card">
+                <div class="product-id"><?php echo $row["product_id"]; ?></div>
+                <div class="product-name"><?php echo $row["product_name"]; ?></div>
+                <div class="product-price"><?php echo $row["product_price"]; ?></div>
+                <div class="product-stock"><?php echo ($row["product_stock"] > 0) ? "In Stock" : "Out of Stock"; ?></div>
+                <img src="<?php echo $row["product_image"]; ?>" alt="Product Image">
+                <div class="product-details"><?php echo $row["product_details"]; ?></div>
+                <a class="buy-now" href="purchase.php?buy&productid=<?php echo $row['product_id']; ?>">Buy Now</a>
+            </div>
         <?php
         }       
         ?>
-    </table>
+    </div>
     <div>
         <ul class="nav-links">
             <li class="btn"><a href="cart.php">Cart</a></li>
@@ -133,5 +159,19 @@ session_start();
     
     <?php @include 'footer.php'; ?>
     
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+     crossorigin="anonymous"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/nouislider.min.js"></script>
+    <script src="js/countdown.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+    <script src="js/gmaps.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
