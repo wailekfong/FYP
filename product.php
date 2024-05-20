bf7119cad894770bbbc85e7897d13569908e9106
wailekfong <149465823+wailekfong@users.noreply.github.com>
2024-05-20 15:10:53 +0800

    img

--- product.php
+++ product.php
@@ -68,7 +68,7 @@ session_start();
         <div class="main_menu">
             <nav class="navbar navbar-expand-lg navbar-light main_box">
                 <div class="container">
-                    <a class="navbar-brand logo_h" href="index.php"><img src="image/dreams3.png" alt=""></a>
+                    <a class="navbar-brand logo_h" href="index.php"><img src="dreams3.png" alt=""></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="icon-bar"></span>
